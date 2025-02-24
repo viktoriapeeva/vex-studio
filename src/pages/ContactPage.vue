59e774d5de0fd3ee234bdae18308280b9f352388
viktoriapeeva <viktoria.peeva02@gmail.com>
2025-02-24 15:23:57 +0200

    Contact page design initial

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -4,26 +4,56 @@
       <!-- <q-img fit="cover" src="~assets/background.png" alt="Vex Studio"></q-img> -->
     </div>
 
-    <div class="page-content">
+    <div>
       <q-page-container>
-        <!-- Send an inquiry -->
-        <h5 class="centered-text title-contacts">Send us an Inquiry</h5>
-        <div class="row">
-          <div class="col send-message-icons">
-            <div>
-              <h6>Or contact us another way:</h6>
+        <div class="contact-form-container">
+          <h5 class="centered-text title-contacts">Send us an Inquiry</h5>
+          <div class="row">
+            <div class="col contact-form-icons">
+              <div>
+                <h6>Get in touch with us</h6>
+              </div>
+              <div>
+                <q-btn icon="mail" flat size="xl" @click="window.location.href = 'mailto:'" />
+                <q-btn icon="send" flat size="xl" @click="window.location.href = 'mailto:'" />
+                <q-btn icon="phone" flat size="xl" @click="window.location.href = 'mailto:'" />
+              </div>
             </div>
-            <div>
-              <q-btn icon="mail" flat size="xl" @click="window.location.href = 'mailto:'" />
-              <q-btn icon="send" flat size="xl" @click="window.location.href = 'mailto:'" />
-              <q-btn icon="phone" flat size="xl" @click="window.location.href = 'mailto:'" />
+            <div class="contact-form col-6 text-center">
+              <q-input
+                dark
+                outlined
+                rounded
+                label-color="secondary"
+                color="secondary"
+                label="Name"
+              />
+              <q-input
+                dark
+                outlined
+                rounded
+                label-color="secondary"
+                color="secondary"
+                label="Email"
+              />
+              <q-input
+                dark
+                outlined
+                rounded
+                label-color="secondary"
+                type="textarea"
+                color="secondary"
+                label="Message"
+              />
+              <q-btn
+                rounded
+                size="md"
+                label="Send"
+                color="secondary"
+                text-color="primary"
+                icon="send"
+              ></q-btn>
             </div>
-          </div>
-          <div class="contact-form col-6">
-            <q-input outlined rounded bg-color="secondary" label="Name" />
-            <q-input outlined rounded bg-color="secondary" label="Email" />
-            <q-input outlined rounded bg-color="secondary" type="textarea" label="Message" />
-            <q-btn rounded size="md" label="Send" color="accent" icon="send"></q-btn>
           </div>
         </div>
       </q-page-container>
